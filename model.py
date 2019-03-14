6c89d37c0efa45f2168e06a960ca010850e791dc
Lei Shi <zhaozhaoqueue@gmail.com>
2019-03-14 19:56:57 -0400

    init

---
+++ model.py
@@ -0,0 +1,24 @@
+def gan_model():
+	# Generator
+	g = Sequential()
+	g.add(Dense(256,input_dim = z_dim))
+	g.add(Activation("relu"))
+	g.add(Dense(784, activation='sigmoid')) 
+	g.compile(loss='binary_crossentropy', optimizer=adam, metrics=['accuracy'])
+
+	# Discrinimator
+	d = Sequential()
+	d.add(Dense(256))
+	d.add(Activation("relu"))
+	d.add(Dense(1, activation='sigmoid'))
+	d.compile(loss='binary_crossentropy', optimizer=adam, metrics=['accuracy'])
+
+	# GAN
+	d.trainable = False
+	inputs = Input(shape=(z_dim, ))
+	hidden = g(inputs)
+	output = d(hidden)
+	gan = Model(inputs, output)
+	gan.compile(loss='binary_crossentropy', optimizer=adam, metrics=['accuracy'])
+
+	return g
